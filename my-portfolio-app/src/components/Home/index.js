167f40c7ea2159a5892e8c879c1d011c074b8dda
Azarill <heierms@gmail.com>
2022-08-02 03:29:00 -0500

    worked on mobile responsiveness

--- my-portfolio-app/src/components/Home/index.js
+++ my-portfolio-app/src/components/Home/index.js
@@ -1,53 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import './index.scss';
-import spaceManSwingImage from '../../assets/images/SpaceSwing.png';
-import AnimatedLetters from '../AnimatedLetters'
-import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+/** @format */
 
+import React, { useEffect, useState } from "react";
+import "./index.scss";
+import spaceManSwingImage from "../../assets/images/SpaceSwing.png";
+import AnimatedLetters from "../AnimatedLetters";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { NavLink } from "react-router-dom";
 
-const Home = () =>{
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['S','c','o','t','t',' '];
-    
-   
-    useEffect(() => {
-         setTimeout(() => {
-          setLetterClass('text-animate-hover')
-        }, 4000)
-      }, [])
+const Home = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArray = ["S", "c", "o", "t", "t", " "];
 
-    return (
-        <section className='hero-section d-flex justify-content-center align-items-center vh-100'>
-        <div className=" d-flex align-items-center justify-content-center">
-          {/* Main Hero content */}
-            <div className="hero-content d-flex flex-column align-items-center" >
-            <h1>
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i,&nbsp; </span>
-               
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span>&nbsp;
-                <AnimatedLetters letterClass={letterClass} 
-                                 strArray={nameArray}
-                                 idx = {15} />
-                                 
-                </h1>
-                <h2>FullStack Developer / JavaScript Expert</h2>
-                <div className='contact-btns'>
-                    <button className='contact-me-btn mt-3'>Contact me</button>
-                    <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/scott-heier-224a40224/'>
-                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
-                    </a>
-                </div>
-            </div>
-            <img src={spaceManSwingImage} alt ='man is spacesuit'/>
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 4000);
+  }, []);
+
+  return (
+    <section className="hero-section">
+      <div className=" d-flex-row pt-5">
+        {/* Main Hero content */}
+        <div className="hero-content d-flex flex-column align-items-center col-sm-12 col-lg-12">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,&nbsp; </span>
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>&nbsp;
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+          </h1>
+          <h2>FullStack Developer / JavaScript Expert</h2>
+          <div className="contact-btns d-flex-row">
+            <NavLink exact="true" activeclassname="active" to="/contact">
+              <button className="contact-me-btn mt-3">Contact me</button>
+            </NavLink>
+            <span className="contact-span">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/scott-heier-224a40224/"
+              >
+                <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/Azariill/"
+              >
+                <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+              </a>
+            </span>
+          </div>
         </div>
-           {/* divs for the animated stars/particles */}
-           <div id="stars"></div>
-            <div id="stars2"></div>
-            <div id="stars3"></div>
-        </section>
-    )
+        <img
+          className="col-sm-12 col-md-2"
+          src={spaceManSwingImage}
+          alt="man is spacesuit"
+        />
+      </div>
+      {/* divs for the animated stars/particles */}
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </section>
+  );
 };
 
 export default Home;
--- my-portfolio-app/src/components/NavBar/index.js
+++ my-portfolio-app/src/components/NavBar/index.js
@@ -1,22 +1,25 @@
 import './index.scss';
+import {NavLink} from 'react-router-dom';
 
 const NavBar = () =>{
 
     return(
         <nav className="navbar navbar-expand-sm">
             <a className="navbar-brand" href="/"><h1 className='m-1 px-3'>Scott Heier</h1></a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            
-        <div className="collapse navbar-collapse  justify-content-end" id="navbarNavAltMarkup">
+          
             <div className="navbar-nav px-5">
-                <a className="nav-item nav-link px-5 " href="#">Projects</a>
-                <a className="nav-item nav-link px-5 "  href="#">About</a>
-                <a className="nav-item nav-link px-5 " href="#">Contact</a>
+            <NavLink exact="true" activeclassname="active" style={{textDecoration: 'none'}} to="/about">
+                <span className="nav-item nav-link px-5 ">About Me</span>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" style={{textDecoration: 'none'}} to="/projects">
+                <span className="nav-item nav-link px-5 " >Projects</span>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" style={{textDecoration: 'none'}} to="/contact">
+                <span className="nav-item nav-link px-5 " >Contact</span>
+            </NavLink>
       
             </div>
-        </div>
+        
 </nav>
     )
 };
