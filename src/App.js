9683d66dcbca2b192818cbb9095d3cc756d71797
Azarill <heierms@gmail.com>
2022-08-29 21:51:17 -0500

    updated css for mobile devices on home page as well as small code refactor

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Home from './components/Home';
-import NoMatch from './components/Layout/NoMatch';
-import Layout from './components/Layout';
+import NoMatch from './components/NoMatch';
 import Contact from './components/ContactForm';
 import Projects from './components/Projects';
 import AboutMe from './components/AboutMe';
 import Resume from './components/Resume';
+import NavBar from './components/NavBar';
 
 
 function App() {
-
+  const [currentNavItem, setCurrentNavItem] = useState('Home');
+  const [navSelected, setNavSelected] =useState('About');
   
   
 
   return (
             <>
+            <main className='background'>
+              <NavBar 
+                currentNavItem={currentNavItem} 
+                setCurrentNavItem={setCurrentNavItem}
+                navSelected={navSelected}
+                setNavSelected={setNavSelected}
+              />
               <Routes>
-                <Route path="/" element={<Layout/>}>
-                  <Route  index element={<Home />} />
+                  <Route path="/" element={<Home/>}/>
                   <Route path='/contact' element={<Contact/>}/>
                   <Route path='/about' element={<AboutMe/>}/>
                   <Route path='/projects' element={<Projects/>}/>
                   <Route path='/resume' element={<Resume/>}/>
-                  <Route path="*" element={<Home/>} />
-                </Route>
+                  <Route path="*" element={<NoMatch/>} />
+              
               </Routes>
                 {/* divs for the animated stars/particles */}
                 <div id="stars"></div>
                 <div id="stars2"></div>
                 <div id="stars3"></div>
+            </main>
             </>
   );
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,49 +2,43 @@
 
 import React, { useEffect, useState } from "react";
 import "./index.scss";
-import spaceManSwingImage from "../../assets/images/SpaceSwing.png";
 import AnimatedLetters from "../AnimatedLetters";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 import Loader from "react-loaders";
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
   const nameArray = ["H","i,"," ", "I'", "m","","S", "c", "o", "t", "t", " "];
-  const navigate = useNavigate();
   useEffect(() => {
     setTimeout(() => {
       setLetterClass("text-animate-hover");
     }, 4000);
   }, []);
 
-  useEffect(() =>{
-    setTimeout(() =>{
-     navigate('/about');
-
-    }, 23000)
-  },[navigate]);
+ 
 
   return (
     <>
     <section className="hero-section d-flex align-items-center justify-content-center">
+
       <div className=" d-flex flex-row flex-wrap justify-content-center align-items-center mt-5 col-12">
         {/* Main Hero content */}
-        <div className="hero-content d-flex flex-column align-items-center  col-sm-12 col-md-4 ">
-          <h1>
+        <div className="hero-content d-flex flex-column align-items-center  col-sm-12 col-md-6 ">
+          <h1 className="h1-home">
             <AnimatedLetters
               letterClass={letterClass}
               strArray={nameArray}
               idx={15}
             />
           </h1>
-          <h2>FullStack Developer / JavaScript Expert</h2>
+          <h2 className="h2-home">FullStack Developer / JavaScript Expert</h2>
           <div className="contact-btns d-flex-row">
             <NavLink exact="true" activeclassname="active" to="/contact">
               <button className="contact-me-btn mt-3">Contact me</button>
             </NavLink>
-            <span className="contact-span">
+            <span className="contact-span ">
               <a
                 target="_blank"
                 rel="noreferrer"
@@ -62,13 +56,7 @@ const Home = () => {
             </span>
           </div>
         </div>
-        <div className='img-div  col-sm-12 col-md-3 '>
-        <img
-          className="floatingImg col-sm-12 col-md-2"
-          src={spaceManSwingImage}
-          alt="man is spacesuit"
-        />
-        </div>
+   
       </div>
     </section>
     <Loader type="pacman" />
